46e87f5f070361c7505ed3044a562c6dedb5d83e
archer <archer.cheng@msa.hinet.net>
2019-04-04 21:32:40 +0800

    admin 6

--- PartySpa/src/app/app.module.ts
+++ PartySpa/src/app/app.module.ts
@@ -1,21 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';
-import { HttpClientModule } from '@angular/common/http';
+// import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+// import { RouterModule } from '@angular/router';
+// import { HttpClientModule } from '@angular/common/http';
 
 import { JwtModule } from '@auth0/angular-jwt';
 import { CollapseModule } from 'ngx-bootstrap/collapse';
 
-// import { SharedAngularMaterialModule } from './_shared/shared-angular-material/shared-angular-material.module';
 import { SharedModule } from './_shared/shared.module';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-import { DynamicFormModule } from './_shared/dynamic-form/dynamic-form.module';
-// import { NavComponent } from './_shared/nav/nav.component';
 
 export function tokenGetter() {
   return localStorage.getItem('token');
@@ -25,18 +22,15 @@ export function tokenGetter() {
   declarations: [
     AppComponent,
     HomeComponent,
-    // NavComponent,
   ],
   imports: [
     BrowserModule,
     CommonModule,
+    SharedModule,
     // FormsModule,
     // ReactiveFormsModule,
     // RouterModule,
     // HttpClientModule,
-    // SharedAngularMaterialModule,
-    SharedModule,
-    DynamicFormModule,
     AppRoutingModule,
     CollapseModule.forRoot(),
     JwtModule.forRoot({
