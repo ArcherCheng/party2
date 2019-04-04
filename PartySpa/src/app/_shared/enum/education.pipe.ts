a30bd8828bcf1e3742cbfa9732a3129ab9e30c31
archer <archer.cheng@msa.hinet.net>
2019-04-04 18:17:51 +0800

    admin 5

--- PartySpa/src/app/_shared/enum/education.pipe.ts
+++ PartySpa/src/app/_shared/enum/education.pipe.ts
@@ -34,3 +34,5 @@ export class EducationPipe implements PipeTransform {
   }
 
 }
+
+
--- PartySpa/src/app/_shared/shared.module.ts
+++ PartySpa/src/app/_shared/shared.module.ts
@@ -4,16 +4,16 @@ import { RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { PaginationModule, TabsModule } from 'ngx-bootstrap';
+import { NgxGalleryModule } from 'ngx-gallery';
+import { FileUploadModule } from 'ng2-file-upload';
 
-import { EducationPipe } from 'src/app/_shared/pipe/education.pipe';
-import { MarryPipe } from 'src/app/_shared/pipe/marry.pipe';
-import { SexPipe } from 'src/app/_shared/pipe/sex.pipe';
 import { MarryConditionPipe } from './pipe/marry-condition.pipe';
+import { MarryPipe } from 'src/app/_shared/pipe/marry.pipe';
+import { EducationPipe } from 'src/app/_shared/pipe/education.pipe';
+import { SexPipe } from 'src/app/_shared/pipe/sex.pipe';
 
 import { PartyCardComponent } from './component/party-card/party-card.component';
 import { MemberCardComponent } from './component/member-card/member-card.component';
-import { NgxGalleryModule } from 'ngx-gallery';
-import { FileUploadModule } from 'ng2-file-upload';
 
 @NgModule({
   declarations: [
